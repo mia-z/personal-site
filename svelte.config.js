57240686d3717b8e1f13fc902ea1ac78128b65c1
mia-z <iperish.x@gmail.com>
2023-01-25 13:13:31 +0100

    changed to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
+import vercel from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
 	},
 };
 
