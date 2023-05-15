01078a9ec4a74d04d67cc5a6fa626716a5102719
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-05-15 12:40:29 +0200

    main front page layout should be done i think

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -14,6 +14,16 @@
         loaded = true;
     });
 
+    let hoveredSide: "left" | "right" | "none" = "none";
+
+    const onSideHover = (side: "left" | "right") => {
+        hoveredSide = side;
+    }
+
+    const offSideHover = () => {
+        hoveredSide = "none";
+    }
+
 </script>
 
 <div in:fade class={""} id={"parallax-root"}>
@@ -23,11 +33,11 @@
         <div id="stars3"></div>
         <div id="stars4"></div>
     </div>
-    <div class={"relative flex flex-row flex-wrap min-h-screen"} id={"parallax"}>
-        <div class={"origin-left w-1/2 bg-transparent z-10 transition-all hover:scale-110 hover:z-20"}>
+    <div class={"relative min-h-screen"} id={"parallax"}>
+        <div on:mouseenter={() => onSideHover("left")} on:mouseleave={() => offSideHover()} class={`glass absolute top-0 left-0 right-1/2 h-full origin-left bg-transparent z-10 transition-all hover:right-[46%] ${hoveredSide === "right" ? "right-[54%]" : ""}`}>
             &nbsp;
         </div>
-        <div class={"origin-right w-1/2 bg-transparent z-10 transition-all hover:scale-110 hover:z-20"}>
+        <div on:mouseenter={() => onSideHover("right")} on:mouseleave={() => offSideHover()} class={`glass absolute top-0 right-0 left-1/2 h-full origin-right bg-transparent z-10 transition-all hover:left-[46%] ${hoveredSide === "left" ? "left-[54%]" : ""}`}>
             &nbsp;
         </div>
         <img draggable="false" src={"/me.jpg"} alt="omg me" class={"rounded-full h-96 morphing-shadow w-96 bg-white absolute top-[calc(50%-182px)] left-[calc(50%-182px)] z-30"} />
@@ -37,7 +47,7 @@
 
 <style>
     .glass {
-        background: rgba(255, 255, 255, 0.11);
+        background: rgba(255, 255, 255, 0.05);
         box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
         backdrop-filter: blur(2.3px);
         -webkit-backdrop-filter: blur(2.3px);
