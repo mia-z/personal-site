7382cd690b433960735675ee2d1107453b06604d
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-05-17 15:29:34 +0200

    add responsiveness to main

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class={"flex flex-col relative z-10"}>
-    <img draggable="false" src={"/me.jpg"} alt={"omg me"} class={"morphing-shadow mx-auto mt-20 h-96 w-96"} />
+    <img draggable="false" src={"/me.jpg"} alt={"omg me"} class={"morphing-shadow mx-auto mt-3 md:mt-5 lg:mt-15 2xl:mt-20 w-3/4 md:h-64 md:w-64 lg:w-96 lg:h-96"} />
     <div class={"prose flex flex-col mt-10 mx-auto"}>
         <h1 class={" text-center wix-madefor-text drop-shadow-lg"}><span class={"bg-clip-text bg-gradient-to-r from-indigo-600 to-fuchsia-300 text-transparent"}>R</span>yan/miaz</h1>
         <hr class={"my-3"} />
@@ -21,7 +21,6 @@
                     <Fa class={"hover:text-fuchsia-400 transition-all text-3xl"} icon={faLinkedin} />
                 </a>
             </ul>
-            
         </li>
     </div>
 </div>
