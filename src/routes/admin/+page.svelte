3926da0239169221f7a6cf984a0cc77ebf6d2a02
Ryan <iperish.x@gmail.com>
2023-09-02 23:44:55 +0200

    login page finished

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,22 +2,48 @@
     import { trpc } from "$lib/trpc/client";
     import { page } from "$app/stores";
     import * as Cookie from "es-cookie";
+    import { ProgressRadial } from "@skeletonlabs/skeleton";
+    import { slide } from "svelte/transition";
+  import { goto } from "$app/navigation";
 
     let password: string = "";
     let loading: boolean = false;
+    let error: string;
 
-    const onPasswordInput = async (e: KeyboardEvent) => {
+    const onPasswordInput = (e: KeyboardEvent) => {
+        if (password == "") {
+            error = "";
+        }
         if (e.key === "Enter") {
             loading = true;
-            const loginRes = await trpc($page).admin.login.mutate(password);
-            Cookie.set("_t", loginRes);
+            trpc($page).admin.login.mutate(password)
+                .then(res => {
+                    Cookie.set("_t", res);
+                    goto("/admin/start");
+                })
+                .catch(e => {
+                    if (e.message === "UNAUTHORIZED") {
+                        error = "Incorrect password"
+                    } else {
+                        error = "Login error";
+                    }
+                });
+            loading = false;
         }
     }
 </script>
 
-<div class={"m-auto w-96 p-5 bg-gradient-to-br variant-gradient-secondary-tertiary shadow-lg rounded-full"}>
+<div class={"m-auto w-96 p-1 bg-gradient-to-br variant-gradient-secondary-tertiary shadow-lg rounded-full relative"}>
     <div class={"input-group input-group-divider grid-cols-[auto_1fr_auto]"}>
         <div class="input-group-shim">Password</div>
-        <input bind:value={password} on:keydown={onPasswordInput} class={""} type="password" />
+        <input disabled={loading} readonly={loading} bind:value={password} on:keydown={onPasswordInput} class={``} type="password" />
+        {#if loading}
+            <div transition:slide={{ axis: "x" }} class={"input-group-shim"}><ProgressRadial stroke={100} meter="stroke-tertiary-500" class={"h-5 w-5"} /></div>
+        {/if}
     </div>
+    {#if error}
+        <div transition:slide class={"absolute top-14 left-0 w-full text-center text-sm text-error-400"}>
+            {error}
+        </div>
+    {/if}
 </div>
