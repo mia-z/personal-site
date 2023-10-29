c6c0ce9fac43ada8b0fd98b7550746e31b06b9c0
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-10-29 15:39:01 +0100

    update yarn to 4, test discord embed stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,12 @@
     <title>
         miaz dev
     </title>
+    <meta content="Miaz dev" property="og:title" />
+    <meta content="Miaz dev stuff" property="og:description" />
+    <meta content="https://miaz.dev" property="og:url" />
+    <meta content="https://static.wikia.nocookie.net/dat-meme-wiki/images/6/68/YeE.jpeg/revision/latest/scale-to-width-down/241?cb=20190502181402" property="og:image" />
+    <meta content="#43B581" data-react-helmet="true" name="theme-color" />
+
 </svelte:head>
 
 <slot />
